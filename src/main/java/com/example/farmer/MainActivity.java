574f9d2bbd8f7f928f6cda8edb73aa4dcadf53f9
Chenziquan <Chenjunquan0208@gmail.com>
2016-05-12 20:58:35 +0800

    cjq

--- src/main/java/com/example/farmer/MainActivity.java
+++ src/main/java/com/example/farmer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.farmer;
+﻿package com.example.farmer;
 
 
 import android.os.Bundle;
@@ -15,6 +15,7 @@ import com.example.farmer.fragment.SortFragment;
 import com.example.farmer.fragment.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
+///cjq
     private static final int HOME = 0;
     private static final int EVENT = 1;
     private static final int SORT = 2;
