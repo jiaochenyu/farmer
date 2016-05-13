d8f72173d7cd606fa8fd2367ec696a7e8f504236
jcy <175687482@qq.com>
2016-05-13 16:03:12 +0800

    矫晨瑜在 hjcActivity文件中增加了一个 jcy()函数

--- src/main/java/com/example/farmer/fragment/HJCActivity.java
+++ src/main/java/com/example/farmer/fragment/HJCActivity.java
@@ -2,6 +2,7 @@ package com.example.farmer.fragment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.farmer.R;
 
@@ -11,5 +12,11 @@ public class HJCActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hjc);
+        jcy();
     }
+
+    private void jcy() {
+        Toast.makeText(HJCActivity.this, "JCY", Toast.LENGTH_SHORT).show();
+    }
+
 }
