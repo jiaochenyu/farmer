f2db75cf1cb9aac0f500101f7ac165c9fdf913e0
hjc <huangjiechun@huangjiechundeMacBook-Pro.local>
2016-05-16 10:59:59 +0800

    jqchen

--- src/main/java/com/example/farmer/fragment/SortFragment.java
+++ src/main/java/com/example/farmer/fragment/SortFragment.java
@@ -4,25 +4,60 @@ package com.example.farmer.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.farmer.MainActivity;
 import com.example.farmer.R;
+import com.example.farmer.fragment.sort.ContentFragment;
+import com.example.farmer.fragment.sort.TitleFragment;
 
 /**
  * Created by jcy on 2016/5/8.
  */
-public class SortFragment extends Fragment {
+public class SortFragment extends Fragment implements TitleFragment.ListItemClick{
     MainActivity mMainActivity;
     View view;
-
+    ListView mListView;
+    //处理事务
+    FragmentManager mFragmentManager;
+    FragmentTransaction mTransaction;
+    ContentFragment mContentFragment;
+    TitleFragment mTitleFragment;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMainActivity = (MainActivity) getActivity();
         view = inflater.inflate(R.layout.sort,null);
+        mListView = (ListView) view.findViewById(R.id.sort_title);
+        initFragment();
         return view;
     }
+
+    private void initFragment() {
+        mFragmentManager = getChildFragmentManager();
+        mTransaction = mFragmentManager.beginTransaction();
+        mContentFragment = new ContentFragment();
+        mTitleFragment = new TitleFragment();
+        mTransaction.add(R.id.right,mContentFragment);
+        mTransaction.add(R.id.left,mTitleFragment);
+        mTransaction.commit();
+    }
+
+    @Override
+    public void setOnListItemClickListener(String text) {
+        mTransaction = mFragmentManager.beginTransaction();
+        mContentFragment = new ContentFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("values",text);
+        mContentFragment.setArguments(bundle);
+        mTransaction.replace(R.id.right,mContentFragment);
+        mTransaction.commit();
+    }
+
 }
