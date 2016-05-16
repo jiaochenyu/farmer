13f835145555c98d96b03ee8373322242afaaa5b
Chenziquan <Chenjunquan0208@gmail.com>
2016-05-16 20:28:57 +0800

    Merge branch 'master' of https://github.com/jiaochenyu/farmer
    # Conflicts:
    #	farmer.iml
    #	src/main/java/com/example/farmer/MainActivity.java
    #	src/main/res/layout/sort.xml
    #	src/main/res/values/strings.xml

--- src/main/java/com/example/farmer/fragment/SortFragment.java
+++ src/main/java/com/example/farmer/fragment/SortFragment.java
@@ -6,53 +6,100 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import com.example.farmer.MainActivity;
 import com.example.farmer.R;
+import com.example.farmer.adapter.Sort_TitleAdapter;
 import com.example.farmer.fragment.sort.ContentFragment;
+import com.example.farmer.utils.DividerItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jcy on 2016/5/8.
  */
 public class SortFragment extends Fragment{
-    MainActivity mMainActivity;
     View view;
-    ListView mListView;
+    RecyclerView mRecyclerView;
+    //适配器
+    Sort_TitleAdapter titleAdapter;
     //处理事务
+    List<String> mList;
+    LinearLayoutManager liner;
     FragmentManager mFragmentManager;
     FragmentTransaction mTransaction;
     ContentFragment mContentFragment;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mMainActivity = (MainActivity) getActivity();
         view = inflater.inflate(R.layout.sort,null);
-        mListView = (ListView) view.findViewById(R.id.sort_title);
+        //初始化数据
+        initData();
+        //初始化适配器
+        initView();
+        //这是监听
+        initAdapter();
+        //初始化布局
         initListener();
+        //初始化碎片
         initFragment();
         return view;
     }
 
+    private void initAdapter() {
+        titleAdapter = new Sort_TitleAdapter(mList);
+        mRecyclerView.setAdapter(titleAdapter);
+    }
+
+    private void initData() {
+        mList = new ArrayList<>();
+        mList.add("蔬菜豆菇");
+        mList.add("新鲜蔬果");
+        mList.add("鲜肉蛋类");
+        mList.add("水产海鲜");
+        mList.add("速冻冻品");
+        mList.add("牛奶面点");
+        mList.add("粮油副食");
+        mList.add("零食酒水");
+        mList.add("进口食品");
+        mList.add("厨房用品");
+    }
+
+    private void initView() {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.sort_title);
+        liner = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(liner);
+        //如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL_LIST));
+    }
+
     private void initListener() {
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        titleAdapter.setmOnItemClickListener(new Sort_TitleAdapter.OnRecyclerViewItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String text = parent.getItemAtPosition(position).toString();
+            public void onItemClick(View view, String data) {
+                //Log.e("jqchen",data);
+                show(data);
                 mTransaction = mFragmentManager.beginTransaction();
                 mContentFragment = new ContentFragment();
 
                 Bundle bundle = new Bundle();
-                bundle.putString("values",text);
+                bundle.putString("values",data);
                 mContentFragment.setArguments(bundle);
                 mTransaction.replace(R.id.right,mContentFragment);
                 mTransaction.commit();
             }
         });
+    }
+
+    private void show(String text) {
+        Toast.makeText(getContext(),"内容："+text,Toast.LENGTH_SHORT).show();
     }
 
     private void initFragment() {
