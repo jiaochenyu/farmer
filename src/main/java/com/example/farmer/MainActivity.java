1804843cdd0b7e169a7d673d256de16b34552a9a
weixiao <734474820@qq.com>
2016-05-16 16:36:59 +0800

    cfj

--- src/main/java/com/example/farmer/MainActivity.java
+++ src/main/java/com/example/farmer/MainActivity.java
@@ -13,12 +13,11 @@ import com.example.farmer.fragment.SortFragment;
 import com.example.farmer.fragment.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
-    //hjcjjjjjj
-    private static final int HOME = 0;
-    private static final int EVENT = 1;
-    private static final int SORT = 2;
-    private static final int CAR = 3;
-    private static final int USER = 4;
+    private static final int HOME = 1;
+    private static final int EVENT = 2;
+    private static final int SORT = 3;
+    private static final int CAR = 4;
+    private static final int USER = 5;
     RadioGroup mRadioGroup;
     FrameLayout mFrameLayout;
 
