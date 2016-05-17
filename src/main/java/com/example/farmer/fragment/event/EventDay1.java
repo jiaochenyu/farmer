39b607bb923d0c63a30f73834d46e91e65ac5223
jcy <175687482@qq.com>
2016-05-18 00:01:49 +0800

    活动页面大致效果已经有了

--- src/main/java/com/example/farmer/fragment/event/EventDay1.java
+++ src/main/java/com/example/farmer/fragment/event/EventDay1.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.farmer.MainActivity;
 import com.example.farmer.R;
@@ -17,11 +18,19 @@ import com.example.farmer.R;
 public class EventDay1 extends Fragment {
     MainActivity mMainActivity;
     View view;
+    TextView mTextView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMainActivity = (MainActivity) getActivity();
         view = inflater.inflate(R.layout.event_day1,null);
+        initViews();
         return view;
+    }
+
+    private void initViews() {
+        mTextView = (TextView) view.findViewById(R.id.texttest);
+        Bundle bundle = getArguments();
+        mTextView.setText(bundle.getString("args",""));
     }
 }
--- src/main/java/com/example/farmer/fragment/event/EventDay2.java
+++ src/main/java/com/example/farmer/fragment/event/EventDay2.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.farmer.MainActivity;
 import com.example.farmer.R;
 
@@ -17,11 +20,26 @@ import com.example.farmer.R;
 public class EventDay2 extends Fragment {
     MainActivity mMainActivity;
     View view;
+    ImageView mImageView;
+    Button mButton;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMainActivity = (MainActivity) getActivity();
-        view = inflater.inflate(R.layout.event_day2,null);
+        view = inflater.inflate(R.layout.event_day2, null);
+        mImageView = (ImageView) view.findViewById(R.id.image);
+        mButton = (Button) view.findViewById(R.id.buttontest);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Glide.with(mMainActivity).load("https://s-media-cache-ak0.pinimg.com/564x/47/ab/21/47ab210b9840baa3601e8daefdc412bc.jpg").into(mImageView);
+            }
+        });
+
         return view;
     }
+
+
 }
