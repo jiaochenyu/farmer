f95816d8c92668e5566e80a5ce9984f254ead348
Chenziquan <Chenjunquan0208@gmail.com>
2016-05-18 09:04:26 +0800

    #5.18 陈军权提交

---
+++ src/main/java/com/example/farmer/activity/SortMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.farmer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.farmer.R;
+
+public class SortMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort_menu);
+    }
+}
