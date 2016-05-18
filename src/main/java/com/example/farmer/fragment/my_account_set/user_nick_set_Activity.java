1c0a4931e4188ae9c9467cbf695f366e2ff48806
weixiao <734474820@qq.com>
2016-05-18 08:51:30 +0800

    cfj 账号设置里面的头像设置

---
+++ src/main/java/com/example/farmer/fragment/my_account_set/user_nick_set_Activity.java
@@ -0,0 +1,15 @@
+package com.example.farmer.fragment.my_account_set;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.farmer.R;
+
+public class user_nick_set_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_nick_set_);
+    }
+}
